3b0e9e05fefa7bef0e8ebbcd5f60e46ebb775afc
Sharon97john <110244759+Sharon97john@users.noreply.github.com>
2023-03-14 18:21:59 +0000

    Add files via upload

---
+++ jpmc-task-3/src/DataManipulator.ts
@@ -0,0 +1,36 @@
+import { ServerRespond } from "./DataStreamer";
+
+export interface Row {
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  timestamp: Date;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
+}
+
+export class DataManipulator {
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const price_abc =
+      (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const price_def =
+      (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = price_abc / price_def;
+    const upper_bound = 1 + 0.05;
+    const lower_bound = 1 - 0.05;
+    return {
+      ratio,
+      price_abc,
+      price_def,
+      upper_bound,
+      lower_bound,
+      timestamp:
+        serverResponds[0].timestamp > serverResponds[1].timestamp
+          ? serverResponds[0].timestamp
+          : serverResponds[1].timestamp,
+      trigger_alert:
+        (ratio > upper_bound || ratio < lower_bound) ? ratio : undefined,
+    };
+  }
+}
